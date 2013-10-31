72b93ca84157693c464acdd56cbe2bcbe6b67fc1
AL-STALKER <new.spamm@gmail.com>
2013-10-31 21:19:50 +0200

    Added dynamic path to DB
    Created a func, which return Connection. Path is read from a txt-file.

--- DBadd/src/app/addDB.java
+++ DBadd/src/app/addDB.java
@@ -1,12 +1,33 @@
 package app;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.*;
+import java.util.Properties;
 
 public class addDB {
+	
+	public static Connection getConnection() throws IOException, SQLException {
+		Connection conn = null;
+	   	Properties props = new Properties();
+		InputStreamReader in = new InputStreamReader(new FileInputStream("appProperties.txt"), "UTF-8");
+	   
+		props.load(in); 
+	    
+		in.close();  
+		
+		// uses pair: key = value There is DBConnectionString = jdbc:derby:C:\\Users\\oper4\\J10_workspace\\CM in a file
+	    String connString = props.getProperty("DBConnectionString");
+	    conn = DriverManager.getConnection(connString);
+	    
+	    return conn;
+	}
 
 	public static void main(String[] args) {
 		try{
-			Connection con = DriverManager.getConnection("jdbc:derby:C:\\Users\\oper4\\J10_workspace\\CM");
+			//Connection con = DriverManager.getConnection("jdbc:derby:C:\\Users\\oper4\\J10_workspace\\CM");
+			Connection con = getConnection();
 			Statement stmt = con.createStatement();
 			ResultSet rs = stmt.executeQuery(
 					"SELECT name, charge, minsum FROM merchant WHERE minsum > 100");
@@ -19,7 +40,7 @@ public class addDB {
 			con.close();
 
 			}
-			catch(SQLException ex){
+			catch(SQLException | IOException ex){
 				System.out.println("Error " + ex.getMessage());
 			}
 	}
